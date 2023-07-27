62aff51ac01a914b30520fa9f5ff6dee77e31cec
ariefdrm <ariefdarmawan5522@gmail.com>
2023-07-27 12:59:56 +0700

    update first config

---
+++ lua/config/treesitter.lua
@@ -0,0 +1,22 @@
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	rainbow = {
+		enable = true,
+		disable = { "html" },
+		extended_mode = false,
+		max_file_lines = nil,
+	},
+	autotag = { enable = true },
+	incremental_selection = { enable = true },
+	indent = { enable = true, disable = { "python", "css" } },
+	autopairs = {
+		enable = true,
+	},
+})
